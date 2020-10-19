3871110296bc21d02a23402bab5bc821b838f4ba
kdvalishivli <kdvalishvili@opus2.com>
2020-10-19 22:02:40 +0400

    Project 0

---
+++ Project 0/flask-app/app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return os.environ.get('SERVER_NAME', 'DEFAULT_SERVER')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
