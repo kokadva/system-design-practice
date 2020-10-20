412b8ce87c9e471b0b3e8fa04d949af7c0f8d50b
kdvalishivli <kdvalishvili@opus2.com>
2020-10-20 05:19:24 +0400

    Add project 1, description is present in the readme

---
+++ Project 1/server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import pika
+
+app = Flask(__name__)
+
+
+@app.route('/add-job/<cmd>')
+def add(cmd):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=cmd,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    connection.close()
+    return " [x] Sent: %s" % cmd
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- README.md
+++ README.md
@@ -25,7 +25,10 @@ Run command: `docker-compose up`
 ### Project 1: Asynchronism with message broker 
 
 #### Description:
-Here goes the description
+Simple server with one endpoint receiving commands for asynchronous execution which puts messages to the Rabbit MQ, and 
+a worker app which is listening to the Rabbit MQ and executes command once it's received. This examples is copied from 
+this source: https://medium.com/better-programming/background-processing-with-rabbitmq-python-and-flask-5ca62acf409c
+// TODO add frontend app for receiving async results 
 
 #### Technologies used:
 * Python/Flask
